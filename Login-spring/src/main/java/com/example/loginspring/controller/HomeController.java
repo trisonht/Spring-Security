f6f404dbe55f00a7f4d090ae7d37bf0f3ccd150a
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-07-10 11:16:39 +0700

    logout

--- Login-spring/src/main/java/com/example/loginspring/controller/HomeController.java
+++ Login-spring/src/main/java/com/example/loginspring/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.loginspring.controller;
 
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
     @GetMapping("/home")
     public String showHome(){
-        return "homepage";
+        return "home-page";
     }
 }
